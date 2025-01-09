2644e8d274ee213aa1e9edf7a789725f7d24be55
Yesh <59663147+YeshaswiM@users.noreply.github.com>
2025-01-08 19:16:31 -0800

    Create count-prefix-and-suffix-pairs-i.py

---
+++ Python/count-prefix-and-suffix-pairs-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPrefixAndSuffix(self, str1: str, str2: str) -> bool:
+        if str2.startswith(str1) and str2.endswith(str1): 
+            return True
+        else: 
+            return False
+
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count=0
+        for idx,string in enumerate(words):
+            # For each string, check if it is a prefix and suffix of subsequent strings
+            for restofstring in words[idx+1::]:
+                if len(string)<=len(restofstring):
+                    if self.isPrefixAndSuffix(string,restofstring):
+                        count+=1
+        return count
