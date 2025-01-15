448661140984e6f8fb541fb4a99cd460b1247ecb
Yesh <yeshaswimenghmalani@gmail.com>
2025-01-15 15:28:41 -0800

    Adding unit tests

---
+++ tests/unit/minimize-xor_test.py
@@ -0,0 +1,13 @@
+import unittest
+from python_solutions.minimize_xor import Solution
+
+class TestCase(unittest.TestCase):
+    def test_first(self):
+        self.assertEqual(Solution().minimizeXor(3, 2), 3)
+
+    def test_second(self):
+        self.assertEqual(Solution().minimizeXor(1, 12), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
