ad6215bf8b5e4d5db7e64f88424ec23811d66150
Yesh <yeshaswimenghmalani@gmail.com>
2025-01-15 15:57:24 -0800

    Update minimize_xor_test.py

--- tests/minimize_xor_test.py
+++ tests/minimize_xor_test.py
@@ -3,7 +3,7 @@ from .minimize_xor_dupe import Solution
 
 class TestCase(unittest.TestCase):
     def test_first(self):
-        self.assertEqual(Solution().minimizeXor(3, 2), 3)
+        self.assertEqual(Solution().minimizeXor(3, 5), 3)
 
     def test_second(self):
         self.assertEqual(Solution().minimizeXor(1, 12), 3)
