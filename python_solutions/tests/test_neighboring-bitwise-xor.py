39cbad40c26f8513af91519109c954a5edf53103
Yesh <yeshaswimenghmalani@gmail.com>
2025-01-17 13:42:31 -0800

    Update test_neighboring-bitwise-xor.py

--- python_solutions/tests/test_neighboring-bitwise-xor.py
+++ python_solutions/tests/test_neighboring-bitwise-xor.py
@@ -3,13 +3,13 @@ from ..neighboring_bitwise_xor import Solution
 
 class TestCase(unittest.TestCase):
     def test_first(self):
-        self.assertEqual(Solution().doesValidArrayExist([1,1,0]))
+        self.assertEqual(Solution().doesValidArrayExist([1,1,0]), True)
 
     def test_second(self):
-        self.assertEqual(Solution().doesValidArrayExist([1,1]))
+        self.assertEqual(Solution().doesValidArrayExist([1,1]), True)
 
     def test_third(self):
-        self.assertEqual(Solution().doesValidArrayExist([1,0]))
+        self.assertEqual(Solution().doesValidArrayExist([1,0]), False)
 
 
 if __name__ == "__main__":
